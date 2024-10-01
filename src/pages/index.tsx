801f7a095650e63d315abb6b921db93b22b841b0
Sydney Bao <entrepreneurs@northeastern.edu>
2024-10-01 01:36:53 -0400

    complete assignment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,23 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
 
+//import Profile from './qcomps/profile_mistake';
+// import FirstComponent from './qcomps/firstcomp';
+// import Bio from './qcomps/bios'
+// import TodoList from './qcomps/todos';
+
+import Profile from "./components/profile_props"
+import Gallery from "./qcomps/gallery_props"
 export default function Home() {
   return (
-    <Profile />
+    
+    <div>
+      <Profile />
+      <Gallery />
+      {/* <FirstComponent />
+      <Bio />
+      <TodoList /> */}
+    </div>
   )
 }
